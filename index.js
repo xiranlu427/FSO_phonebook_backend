002a16bff2261cc9a767911af20dbd84116c78ec
Xiran Lu <xlu92427@gmail.com>
2025-07-28 16:39:01 -0700

    Add production build

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
@@ -95,6 +97,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 })
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
